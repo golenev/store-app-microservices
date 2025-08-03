90f78c1be96bbb86cfef725131ac4e86f5b4723b
Голенев Василий <84588235+golenev@users.noreply.github.com>
2025-08-03 11:01:44 +0300

    Map barcodeId to legacy id column

--- cart-service/src/main/java/com/experience_kafka/model/WarehouseProduct.java
+++ cart-service/src/main/java/com/experience_kafka/model/WarehouseProduct.java
@@ -1,5 +1,6 @@
 package com.experience_kafka.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -29,6 +30,7 @@ public class WarehouseProduct {
     @Id
     @NotNull
     @Positive
+    @Column(name = "id")
     private Long barcodeId;
 
     @NotBlank
